1ca533c276b17b2a0e7f03421d41c5f2b28d9019
Shoaib <shoaibkakil@gmail.com>
2021-10-21 10:38:50 +0500

    Update Permutation.cpp
    added comments for a better understanding of code.

--- C++/Permutation.cpp
+++ C++/Permutation.cpp
@@ -6,11 +6,14 @@ using namespace std;
 int main()
 {
  long long n;
+ //take input; n is the number whom permutation has to find
  cin >>n;
  long long p[n];
  int count=0;
  int flag = 0;
  long long i;
+ 
+ // loop to add even numbers inside p[].
  for(i=1; i<=n; i++)
  {
     if(i%2==0)
@@ -19,6 +22,8 @@ int main()
         count++;
     }
  }
+ 
+ // loop to add odd numbers inside p[] 
  for( i=1; i<=n; i++)
  {
     if(i%2!=0)
@@ -30,6 +35,7 @@ int main()
  }
 
 
+ // Now by comparing elements inside p[], we find out if the permutation of n is possible or not.
  for( i=0; i<n-1; i++ )
  {
      if(p[i] == p[i+1]-1 || p[i] == p[i+1]+1 )
@@ -40,6 +46,7 @@ int main()
 
 
 
+ // from above comparision flag=0 means permutation is possible.
  if(flag==0)
  {
     for(long long j=0; j<n; j++)
