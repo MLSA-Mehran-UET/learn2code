724ca118bdec76cf899dee0b3e0e45f481cd7dab
Junaid Ahmed <74968802+JunaidAhmed032@users.noreply.github.com>
2021-10-26 09:46:05 +0500

    Update Decimal to binary Convertor.java

--- Java/Decimal to binary Convertor.java
+++ Java/Decimal to binary Convertor.java
@@ -12,19 +12,23 @@ import java.util.Scanner;
  * @author lenovo
  */
 public class BinaryConvet {
+// BINARY CONVERTER FUNCTION BELOW:            
 public static int BinaryConverter(int Input){
+           //CHECHING IF NUMBER IS ZERO
             if (Input == 0)  
             return 0;  
               
         else
-          
-        return (Input % 2 + 10 *  
-               BinaryConverter(Input / 2));
+          //FROMULA OF CONERTING TO BINARY
+        return (Input % 2 + 10 *  BinaryConverter(Input / 2));
     }
        public static void main(String[] args) {
+          //CREATING SCANNER CLASS OBJECT TO TAKE INPUT
            Scanner s=new Scanner(System.in);
            System.out.println("Enter a decimal number ");
+            //DECIMAL VARIABLE FRO DECIMAL NUMBER
             int DecimalNumber=s.nextInt();
+            //PRINTING AND PASSING ARGUMENTS TO BINARYCONVERTER FUNCTION
             System.out.println("Binary number is :"+BinaryConverter(DecimalNumber));
        
     }
